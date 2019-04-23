473dad3f174036c276aa16160979c01708bb7b1f
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-23 00:58:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,22 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+var price = Math.floor(Math.random() * 101);
+ cart.push(Object.assign({[item]:price}));
+return `${item} has been added to your cart.`;
 }
+ }
+
 
 function viewCart() {
   // write your code here
-}
+ if (cart.length === 0) {
+return `Your shopping cart is empty.`;
+ } 
+ 
+
+
+
 
 function total() {
   // write your code here
